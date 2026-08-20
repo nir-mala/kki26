ba6304e9ee38039319f56a1c5d3cc5201a852127
nir-mala <mariands175@gmail.com>
2026-08-20 12:48:11 +0700

    Update lintasan and gitignore

--- web_app/src/routes/comp/lintasan.jsx
+++ web_app/src/routes/comp/lintasan.jsx
@@ -24,7 +24,7 @@ const Lintasan = ({ namaLintasan, children }) => {
   // =========================================================
 
   const CLOUDFLARE_IMAGE_URL =
-    'https://fare-bulk-monte-installing.trycloudflare.com'
+    'https://snake-solomon-cayman-leaves.trycloudflare.com'
 
   const FALLBACK_SURFACE = '/surface.jpg'
   const FALLBACK_UNDERWATER = '/underwater.jpg'
